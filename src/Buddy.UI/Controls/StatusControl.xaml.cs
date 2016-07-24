72f012546b233a79201b5805191b480a74c9a2a6
Valerio Santinelli <santinelli@altralogica.it>
2016-07-24 16:16:25 +0200

    Proof of Concept for the Pokemon GO Bot

--- src/Buddy.UI/Controls/StatusControl.xaml.cs
+++ src/Buddy.UI/Controls/StatusControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,56 @@ using System.Windows.Shapes;
 
 namespace Buddy.UI.Controls
 {
-    /// <summary>
-    /// Interaction logic for StatusControl.xaml
-    /// </summary>
-    public partial class StatusControl : UserControl
+  /// <summary>
+  /// Interaction logic for StatusControl.xaml
+  /// </summary>
+  public partial class StatusControl : UserControl
+  {
+    private Process p;
+
+    public StatusControl()
     {
-        public StatusControl()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent();
     }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      if (p != null)
+      {
+        MessageBox.Show("Bot is already running", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+      }
+      if (lbProvider.SelectedItem == null)
+      {
+        MessageBox.Show("You must select a provider", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        return;
+      }
+      var provider = ((ListBoxItem)lbProvider.SelectedItem).Content.ToString();
+      var username = tbUsername.Text;
+      var password = tbPassword.Text;
+      var location = tbLocation.Text;
+      var args = $"--provider {provider} -u {username} -p {password} --location \"{location}\" -c";
+      Console.WriteLine(args);
+      try
+      {
+        var path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "pgo-client.exe");
+        p = Process.Start(path, args);
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+      }
+      Console.ReadLine();
+    }
+
+    private void StopButton_Click(object sender, RoutedEventArgs e)
+    {
+      if (p == null)
+      {
+        return;
+      }
+
+      p.Kill();
+      p = null;
+    }
+  }
 }
