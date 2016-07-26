050cdd226f9aaa84281f477760909890a1d304fe
Valerio Santinelli <santinelli@altralogica.it>
2016-07-26 16:15:06 +0200

    Some changes to save basic data and manage the underlying process

--- src/Buddy.UI/Controls/StatusControl.xaml.cs
+++ src/Buddy.UI/Controls/StatusControl.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,10 +24,39 @@ namespace Buddy.UI.Controls
   public partial class StatusControl : UserControl
   {
     private Process p;
+    private bool IsRunning;
 
     public StatusControl()
     {
       InitializeComponent();
+      // Load the current configuration
+      var provider = ConfigurationManager.AppSettings.Get("provider");
+      if (provider != null)
+      {
+        foreach(var item in lbProvider.Items)
+        {
+          if (((ListBoxItem)item).Content.ToString() == provider)
+          {
+            lbProvider.SelectedItem = item;
+          }
+        }
+      }
+      var username = ConfigurationManager.AppSettings.Get("username");
+      if (username != null)
+      {
+        tbUsername.Text = username;
+      }
+      var password = ConfigurationManager.AppSettings.Get("password");
+      if (password != null)
+      {
+        tbPassword.Password = password;
+      }
+      var location = ConfigurationManager.AppSettings.Get("location");
+      if (location != null)
+      {
+        tbLocation.Text = location;
+      }
+
     }
 
     private void Button_Click(object sender, RoutedEventArgs e)
@@ -41,26 +72,60 @@ namespace Buddy.UI.Controls
       }
       var provider = ((ListBoxItem)lbProvider.SelectedItem).Content.ToString();
       var username = tbUsername.Text;
-      var password = tbPassword.Text;
+      var password = tbPassword.Password;
       var location = tbLocation.Text;
+
+      // Save the current configuration
+      Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+      config.AppSettings.Settings["provider"].Value = provider;
+      config.AppSettings.Settings["username"].Value = username;
+      config.AppSettings.Settings["password"].Value = password;
+      config.AppSettings.Settings["location"].Value = location;
+      config.Save(ConfigurationSaveMode.Modified);
+      ConfigurationManager.RefreshSection("appSettings");
+
       var args = $"--provider {provider} -u {username} -p {password} --location \"{location}\" -c";
       Console.WriteLine(args);
       try
       {
         var path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "pgo-client.exe");
-        p = Process.Start(path, args);
+        ProcessStartInfo si = new ProcessStartInfo(path, args);
+        si.WindowStyle = ProcessWindowStyle.Hidden;
+        si.CreateNoWindow = true;
+        si.RedirectStandardOutput = true;
+        si.RedirectStandardError = true;
+        si.RedirectStandardInput = true;
+        si.UseShellExecute = false;
+        p = Process.Start(si);
+        IsRunning = true;
+        logBox.Document.Blocks.Clear();
+        p.OutputDataReceived += (s, ev) =>
+        {
+          this.Dispatcher.Invoke((Action)(() =>
+          {
+            logBox.AppendText(ev.Data);
+          }));
+        };
+
+        p.BeginOutputReadLine();
+        //p.WaitForExit();
       }
       catch (Exception ex)
       {
         MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
       }
-      Console.ReadLine();
     }
 
     private void StopButton_Click(object sender, RoutedEventArgs e)
     {
       if (p == null)
       {
+        return;
+      }
+
+      if (p.HasExited)
+      {
+        p = null;
         return;
       }
 
